201ae261d720fbc64acbe4605ee03ca5b758015e
Flavio Vieira <flavioemerson1@outlook.com>
2023-09-28 14:13:01 -0300

    commit inicial

---
+++ bin/spring_dart.dart
@@ -0,0 +1,5 @@
+import 'package:spring_dart/spring_dart.dart' as spring_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${spring_dart.calculate()}!');
+}
---
+++ test/spring_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:spring_dart/spring_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
